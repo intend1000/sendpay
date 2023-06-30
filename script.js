954e33f6a5097bc7ffca1c24cda964e925f092ba
Nursultan Berbaev <intend1000@gmail.com>
2023-06-30 18:55:30 +0600

    add project

---
+++ script.js
@@ -0,0 +1,38 @@
+$('.carousel').owlCarousel({
+    loop:true,
+    margin:1,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+$('.about-us-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    margin:18,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
